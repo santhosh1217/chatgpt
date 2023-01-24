9babb4a5271d96193ced3658ae324d95d4932f78
santhosh1217 <79699124+santhosh1217@users.noreply.github.com>
2023-01-24 22:43:41 +0530

    Add files via upload

---
+++ chatgpt/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+import openai
+
+openai.api_key = "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"
+
+# Create your views here.
+def chat(question):
+        result = openai.Completion.create(engine="text-davinci-003",prompt=question,    max_tokens=1024,n=1,stop=None,temperature=0.5,)
+        replay = result.choices[0].text
+        return str(replay)
+
+def home(request):
+    if(request.method == 'POST'):
+        res = chat(str(request.POST["question"]))
+
+        return render(request,"index.html",{"name":res})
+    else:
+        return render(request,"index.html",{"name":"josh"})
+    
