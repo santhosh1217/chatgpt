01d1dd9ab5dd75dd0ad511a4d27726446d7f0b27
santhosh1217 <79699124+santhosh1217@users.noreply.github.com>
2023-01-25 09:07:23 +0530

    Update settings.py

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -75,10 +75,7 @@ WSGI_APPLICATION = 'chatbot.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  
 }
 
 
